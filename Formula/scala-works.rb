658e101cc62ebd0f50e371fc38e4dd073bf7bcb6
Mark Rudolph <mark@k8ty.app>
2023-02-11 15:20:27 -0500

    I'm learning SO MUCH today

--- Formula/scala-works.rb
+++ Formula/scala-works.rb
@@ -3,7 +3,7 @@ class ScalaWorks < Formula
   homepage "https://github.com/scala-works/scala-works-cli"
   url "https://github.com/scala-works/scala-works-cli/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "8cd2dddb0d2f36397ec99b83c5922df5acbbf7c2e593ef379660556520fb283a"
-  license "Apache 2"
+  license "Apache-2.0"
 
   depends_on "Virtuslab/scala-cli/scala-cli" => :build
 
