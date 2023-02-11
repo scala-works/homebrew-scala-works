21a44fadcad74c70b270f30b3810a12234b0cb6a
Mark Rudolph <mark@k8ty.app>
2023-02-11 15:45:19 -0500

    scala-works 0.0.1 (new formula)
    * scala-works 0.0.1 (new formula)
    * Try making scala-cli optional
    * revert: Try making scala-cli optional
    * require scala-cli only on build
    * use resource for scala-cli
    * works on my machine
    * 1 file inspected, no offenses detected
    * specify jvm when building
    * try setting java 11
    * try setting java home
    * remove test
    * fix style
    * depend on java 11
    * scala-cli seems to need at least 17
    
    Closes #3.
    
    Signed-off-by: Mark Rudolph <mark@k8ty.app>

---
+++ Formula/scala-works.rb
@@ -0,0 +1,36 @@
+class ScalaWorks < Formula
+  desc "Command-line utility to help with scala-works resources"
+  homepage "https://github.com/scala-works/scala-works-cli"
+  url "https://github.com/scala-works/scala-works-cli/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "8cd2dddb0d2f36397ec99b83c5922df5acbbf7c2e593ef379660556520fb283a"
+  license "Apache-2.0"
+
+  depends_on "openjdk" => :build
+
+  resource "scala-cli" do
+    url "https://github.com/VirtusLab/scala-cli/releases/download/v0.1.20/scala-cli"
+    sha256 "0c070177249524e26e6e13571c240b2d1bdfcc24027fa7d6d4d46ce70c306c24"
+  end
+
+  def install
+    ENV["JAVA_HOME"] = Formula["openjdk"].opt_prefix
+    resource("scala-cli").stage do
+      chmod "+x", "scala-cli"
+      buildpath.install "scala-cli"
+    end
+    system buildpath/"scala-cli", "package", ".", "-o", "sw", "--jvm", "11"
+    bin.install "sw"
+  end
+
+  def caveats
+    <<~EOS
+      This application is packages with scala-cli. It will pick up JAVA_HOME
+      which needs to be at least 11.
+    EOS
+  end
+
+  test do
+    ENV["JAVA_HOME"] = Formula["openjdk"].opt_prefix
+    system bin/"sw"
+  end
+end
