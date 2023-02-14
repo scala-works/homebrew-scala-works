026841d71e5962ba58a2cfd9659d1edee4dd2a77
Mark Rudolph <mark@k8ty.app>
2023-02-14 00:27:02 -0500

    remove actions, update install

--- Formula/scala-works.rb
+++ Formula/scala-works.rb
@@ -1,31 +1,12 @@
 class ScalaWorks < Formula
   desc "Command-line utility to help with scala-works resources"
   homepage "https://github.com/scala-works/scala-works-cli"
-  url "https://github.com/scala-works/scala-works-cli/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "e31a4fa436cf1d13a057bef22e19dc657a6d9a3f2f79bcc0e84f6b5ec82abc65"
+  url "https://github.com/scala-works/scala-works-cli/releases/download/v0.0.1/scala-works-cli-v0.0.1-ubuntu-latest.tar.gz"
+  sha256 "cf4c946018eec75a5d39ea0bd9e2658120d76e59424879bea1c179c2d2a3136e"
   license "Apache-2.0"
 
-  bottle do
-    root_url "https://github.com/scala-works/homebrew-scala-works/releases/download/scala-works-0.0.2"
-    sha256 cellar: :any_skip_relocation, monterey:     "486cb3974c142c7971783212b1c1a41c3899dd19cbe721006874544b7dfa4e4c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "753e0b893a40b0fb225d476148557a44f0dcb8a3f3ee338c508eb08ceafdf10a"
-  end
-
-  depends_on "openjdk" => :build
-
-  resource "scala-cli" do
-    url "https://github.com/VirtusLab/scala-cli/releases/download/v0.1.20/scala-cli"
-    sha256 "0c070177249524e26e6e13571c240b2d1bdfcc24027fa7d6d4d46ce70c306c24"
-  end
-
   def install
-    ENV["JAVA_HOME"] = Formula["openjdk"].opt_prefix
-    resource("scala-cli").stage do
-      chmod "+x", "scala-cli"
-      buildpath.install "scala-cli"
-    end
-    system buildpath/"scala-cli", "package", ".", "-o", "sw", "--jvm", "11"
-    bin.install "sw"
+    bin.install "scala-works-cli" => "sw"
   end
 
   def caveats
@@ -36,7 +17,6 @@ class ScalaWorks < Formula
   end
 
   test do
-    ENV["JAVA_HOME"] = Formula["openjdk"].opt_prefix
     system bin/"sw"
   end
 end
